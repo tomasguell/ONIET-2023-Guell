b32ef04c850d3cbe48c7562b56c277eaf1bac755
Tomas Guell <tomasguell123@gmail.com>
2023-10-26 11:36:42 -0300

    Home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import officeImage from './office.jpg';
-import './HomePage.css';
-import CardPrestamos from './CardPrestamos';
 
-import Container from 'react-bootstrap/Container';
+
+
+
+
+
 
 function HomePage() {
   
   return (
-    <Container className='pagecontainer container'>
+    <div className='pagecontainer container'>
        <h1>Hola</h1>
-    </Container>
+    </div>
   );
 }
 
