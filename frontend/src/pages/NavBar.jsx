201fef9b3b65a9940144930ec34f2d31cb6e5b97
Tomas Guell <tomasguell123@gmail.com>
2023-10-26 11:57:24 -0300

    Navbar

---
+++ frontend/src/pages/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+function NavBar() {
+  return (
+    <Navbar bg="primary" expand="md">  
+    <Container>  
+      <Navbar.Brand href="#home">ITS VILLADA</Navbar.Brand>  
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />  
+      <Navbar.Collapse id="basic-navbar-nav">  
+        <Nav className="me-auto">  
+
+        </Nav>  
+      </Navbar.Collapse>  
+    </Container>  
+  </Navbar>  
+  );
+}
+
+export default NavBar;
