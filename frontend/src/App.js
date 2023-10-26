74ab8b21d4dd18fa3eff895036567f1efec816a4
Tomas Guell <tomasguell123@gmail.com>
2023-10-26 11:34:00 -0300

    Home y Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HomePage from './pages/home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+
+
+    <Router>
+   
+      <div className="container">
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            
+          </Routes>
+        </div>
+      </div>
+    
+  </Router>
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import officeImage from './office.jpg';
+import './HomePage.css';
+import CardPrestamos from './CardPrestamos';
+
+import Container from 'react-bootstrap/Container';
+
+function HomePage() {
+  
+  return (
+    <Container className='pagecontainer container'>
+       <h1>Hola</h1>
+    </Container>
+  );
+}
+
+export default HomePage;
